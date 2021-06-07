608ab0061fcc9ac3d06d775eb7a05e5c8dbd44b6
STATION\MF <mf2199@columbia.edu>
2021-06-06 20:24:40 -0400

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -54,46 +54,46 @@ class TestHandler(unittest.TestCase):
                 address["addressPostalCode"] = fake.postcode()
         return detail
 
-    # def test_ftr_cc_01_unique_residences(self):
-    #     # with open(os.path.join("tests", "loandata.json")) as file:
-    #     #     event = self._generate_event(json.load(file))
-    #     #     for record in event["Records"]:
-    #     #         record["detail"] = json.loads(record["detail"])
-    #     #     logging.info(f"EVENT:\n{json.dumps(event, indent=2)}")
-    #
-    #     with open(LOAN_DATA_FILE) as file:
-    #         event = self._generate_event(json.load(file))
-    #
-    #     response = main(event)
-    #
-    #     # logging.info('REPORTS: %s', json.dumps(response, indent=2))
-    #     for report in response["reports"]:
-    #         if report["title"] == "Residences Report":
-    #             self.assertEqual(len(report["residences"]), 1)
-    #
-    #     event = self._generate_event(detail=self._randomize(event))
-    #
-    #     response = main(event)
-    #     for report in response["reports"]:
-    #         if report["title"] == "Residences Report":
-    #             self.assertEqual(len(report["residences"]), 2)
-    #
-    # def test_ftr_cc_02_shared_address(self):
-    #     with open(LOAN_DATA_FILE) as file:
-    #         event = self._generate_event(json.load(file))
-    #
-    #     response = main(event)
-    #
-    #     for report in response["reports"]:
-    #         if report["title"] == "Borrowers Report":
-    #             self.assertTrue(report["shared_address"])
-    #
-    #     event = self._generate_event(detail=self._randomize(event))
-    #
-    #     response = main(event)
-    #     for report in response["reports"]:
-    #         if report["title"] == "Borrowers Report":
-    #             self.assertFalse(report["shared_address"])
+    def test_ftr_cc_01_unique_residences(self):
+        # with open(os.path.join("tests", "loandata.json")) as file:
+        #     event = self._generate_event(json.load(file))
+        #     for record in event["Records"]:
+        #         record["detail"] = json.loads(record["detail"])
+        #     logging.info(f"EVENT:\n{json.dumps(event, indent=2)}")
+
+        with open(LOAN_DATA_FILE) as file:
+            event = self._generate_event(json.load(file))
+
+        response = main(event)
+
+        # logging.info('REPORTS: %s', json.dumps(response, indent=2))
+        for report in response["reports"]:
+            if report["title"] == "Residences Report":
+                self.assertEqual(len(report["residences"]), 1)
+
+        event = self._generate_event(detail=self._randomize(event))
+
+        response = main(event)
+        for report in response["reports"]:
+            if report["title"] == "Residences Report":
+                self.assertEqual(len(report["residences"]), 2)
+
+    def test_ftr_cc_02_shared_address(self):
+        with open(LOAN_DATA_FILE) as file:
+            event = self._generate_event(json.load(file))
+
+        response = main(event)
+
+        for report in response["reports"]:
+            if report["title"] == "Borrowers Report":
+                self.assertTrue(report["shared_address"])
+
+        event = self._generate_event(detail=self._randomize(event))
+
+        response = main(event)
+        for report in response["reports"]:
+            if report["title"] == "Borrowers Report":
+                self.assertFalse(report["shared_address"])
 
     def test_ftr_cc_03_multiple_applications(self):
         with open(LOAN_DATA_FILE) as file:
