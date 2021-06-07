a9ba7003143a0d560f3a10ba8890ac50127d0372
STATION\MF <mf2199@columbia.edu>
2021-06-06 20:32:38 -0400

    Create noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# from __future__ import absolute_import
+
+import nox
+import os
+
+
+DEFAULT_PYTHON_VERSION = "3.8"
+UNIT_TEST_PYTHON_VERSIONS = ["3.8"]
+
+
+def default(session):
+    session.install("faker", "mock", "pytest", "pytest-cov")
+
+    # Run py.test against the unit tests.
+    session.run(
+        "py.test",
+        "--quiet",
+        "--cov=tests",
+        "--cov-append",
+        # "--cov-config=.coveragerc",
+        "--cov-report=",
+        "--cov-fail-under=0",
+        os.path.join("tests", "test_models.py"),
+        # "tests",
+        *session.posargs,
+    )
+
+
+@nox.session(python=UNIT_TEST_PYTHON_VERSIONS)
+def unit(session):
+    """Run the unit test suite."""
+    default(session)
